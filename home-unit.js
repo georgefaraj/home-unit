7c452149e8aac39097a6a2f53c6ee3e21c94f87c
georgefaraj <george_sci@hotmail.com>
2018-12-04 18:50:19 -0400

    Update home-unit.js

--- home-unit.js
+++ home-unit.js
@@ -1,19 +1,34 @@
-mqtt = require('mqtt')
-client = mqtt.connect('mqtt://broker.hivemq.com')
+var mqtt = require('mqtt')
+var client = mqtt.connect('mqtt://broker.hivemq.com')
 
+var serialport = require('serialport');
+var SerialPort = serialport.SerialPort;
+
+var port = new SerialPort("/dev/ttyACM0", {
+    baudrate: 57600,
+    parser: serialport.parsers.readline("\n")
+});
 
 client.on('connect', () => {
-	client.subscribe("Group12Test/hi")
-	client.subscribe("Group12Test/bye")
+	client.subscribe("Group12Test/Tweety/Post")
+	client.subscribe("Group12Test/Lucibel/On")
+	client.subscribe("Group12Test/Lucibel/Off")
 })
 
 client.on('message', (topic, message) => {
   switch (topic) {
-    case "Group12Test/hi":
+    case "Group12Test/Tweety/Post":
 		console.log(message+"!")
-    case "Group12Test/bye":
-    	console.log(message+"!!!")
+    case "Group12Test/Lucibel/On":
+    	console.log(message+"!")
+    default:
+    	console.log('No handler for topic %s', topic)
   }
-  console.log('No handler for topic %s', topic)
 })
 
+port.on("open", function () {
+    console.log('open');
+    port.on('data', function(data) {
+        console.log(data);
+    });
+});
